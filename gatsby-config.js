dcd966ced535193a5aac67abb8fcfd97e9c268d2
Matt <mattpezzente@gmail.com>
2019-07-18 10:48:38 -0400

    start: imported h31-starter version 0.1.0

---
+++ gatsby-config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  siteMetadata: {
+    title: "[]",
+    description: "[]",
+    author: "[]",
+    siteurl: "[]",
+    social: [
+      {
+        name: "facebook",
+        url: "[]"
+      }
+    ]
+  },
+  plugins: [
+    // File sourcing into Gatsby for use with Gatsby Node API
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`
+      }
+    },
+    // Creates a manifest for the website
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "[]",
+        short_name: "[]",
+        start_url: "/",
+        background_color: "[]",
+        theme_color: "[]",
+        display: "minimal-ui"
+        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      }
+    },
+    // Loads web fonts from Google as specified
+    {
+      resolve: "gatsby-plugin-web-font-loader"
+      // options: {
+      //   google: {
+      //     families: []
+      //   },
+      // },
+    },
+    // Emotion plugin for Gatsby
+    `gatsby-plugin-emotion`,
+
+    // Netlify CMS
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-netlify",
+
+    // Robots TXT
+    "gatsby-plugin-robots-txt",
+
+    // Creates a Service Worker for offline use
+    "gatsby-plugin-offline",
+
+    // Allows the use of react-helmet within Gatsby
+    "gatsby-plugin-react-helmet",
+
+    // Creates ImageSharp nodes for image manipulation within GraphQL queries
+    "gatsby-transformer-sharp",
+
+    // Gives additional image processing functions
+    "gatsby-plugin-sharp",
+
+    // Gatsby Plugin for linting -- Using AirBNB
+    "gatsby-plugin-eslint"
+  ]
+};
---
+++ static/folder-name/test-blog-post.md
@@ -0,0 +1,7 @@
+---
+date: 2019-06-01T15:00:19.044Z]
+title: Example Title
+image: /assets/example-image.jpg
+---
+
+This is where text information goes!
