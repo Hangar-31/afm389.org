7a79ccb82b089c75a9978bfbeb08c2da1450fd4d
Jake <gosnelljacob@gmail.com>
2019-07-30 14:26:36 -0700

    created new TextBlock3 component

---
+++ src/components/h31/text-blocks/TextBlock3.js
@@ -0,0 +1,25 @@
+// import { PropTypes } from "prop-types";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const Container = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const ContentContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+`;
+
+const TextBlock3 = () => (
+  <Container>
+    <ContentContainer />
+  </Container>
+);
+
+TextBlock3.defaultProps = {};
+
+TextBlock3.propTypes = {};
