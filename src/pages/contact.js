ddb2ab5ca3763e0ec2b07f4bd4b6ea9f2f957155
Jake <gosnelljacob@gmail.com>
2019-08-15 12:45:33 -0700

    Set up initial media query architecture for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { css } from "@emotion/core";
+import MediaQuery from "react-responsive";
 
 // Config
 import _config from "../components/_config";
@@ -21,6 +22,8 @@ import {
 // Images
 import ImgMastheadContact from "../components/images/mastheads/ImgMastheadContact";
 
+// Establishment of Responsive Breakpoints
+
 export default () => (
   <Layout>
     <SEO title="" description="" />
@@ -36,69 +39,82 @@ export default () => (
       titleSmall="Central FL Musicians Association"
       titleLarge="Contact Information"
     />
+    <MediaQuery query="(min-width: 992px)">
+      <H31LayoutContainer
+        fluid
+        as="section"
+        css={css`
+          margin: 90px 0;
+        `}
+      >
+        <H31LayoutRow>
+          <H31LayoutCol xs={1} />
+          <H31LayoutCol xs={2}>
+            <H31Title3A
+              css={css`
+                margin-bottom: 15px;
+                color: ${_config.colorSecondary};
+              `}
+            >
+              Mailing Address
+            </H31Title3A>
+            <H31Link4
+              css={css`
+                display: block;
+                margin-bottom: 60px;
+              `}
+              href="https://www.google.com/maps/place/3020+E+Robinson+St,+Orlando,+FL+32803/@28.5457854,-81.3470503,17z/data=!3m1!4b1!4m5!3m4!1s0x88e77ad22dc16cc1:0x85de0e8e8138f6ec!8m2!3d28.5457854!4d-81.3448616"
+            >
+              {["3020 East Robinson Street", <br />, "Orlando, FL 3280"]}
+            </H31Link4>
 
-    <H31LayoutContainer
-      fluid
-      as="section"
-      css={css`
-        margin: 90px 0;
-      `}
-    >
-      <H31LayoutRow>
-        <H31LayoutCol xs={1} />
-        <H31LayoutCol xs={2}>
-          <H31Title3A
-            css={css`
-              margin-bottom: 15px;
-              color: ${_config.colorSecondary};
-            `}
-          >
-            Mailing Address
-          </H31Title3A>
-          <H31Link4
-            css={css`
-              display: block;
-              margin-bottom: 60px;
-            `}
-            href="https://www.google.com/maps/place/3020+E+Robinson+St,+Orlando,+FL+32803/@28.5457854,-81.3470503,17z/data=!3m1!4b1!4m5!3m4!1s0x88e77ad22dc16cc1:0x85de0e8e8138f6ec!8m2!3d28.5457854!4d-81.3448616"
-          >
-            {["3020 East Robinson Street", <br />, "Orlando, FL 3280"]}
-          </H31Link4>
+            <H31Title3A
+              css={css`
+                margin-bottom: 15px;
+                color: ${_config.colorSecondary};
+              `}
+            >
+              Number
+            </H31Title3A>
+            <H31Link4
+              css={css`
+                display: block;
+                margin-bottom: 60px;
+              `}
+              href="tel::(407) 894-8666"
+            >
+              (407) 894-8666
+            </H31Link4>
 
-          <H31Title3A
-            css={css`
-              margin-bottom: 15px;
-              color: ${_config.colorSecondary};
-            `}
-          >
-            Number
-          </H31Title3A>
-          <H31Link4
-            css={css`
-              display: block;
-              margin-bottom: 60px;
-            `}
-            href="tel::(407) 894-8666"
-          >
-            (407) 894-8666
-          </H31Link4>
-
-          <H31Title3A
-            css={css`
-              margin-bottom: 15px;
-              color: ${_config.colorSecondary};
-            `}
-          >
-            E-mail
-          </H31Title3A>
-          <H31Link4 href="mailto::Local389@afm.org">Local389@afm.org</H31Link4>
-        </H31LayoutCol>
-        <H31LayoutCol xs={1} />
-        <H31LayoutCol xs={7}>
-          <H31GoogleMap1 />
-        </H31LayoutCol>
-        <H31LayoutCol xs={1} />
-      </H31LayoutRow>
-    </H31LayoutContainer>
+            <H31Title3A
+              css={css`
+                margin-bottom: 15px;
+                color: ${_config.colorSecondary};
+              `}
+            >
+              E-mail
+            </H31Title3A>
+            <H31Link4 href="mailto::Local389@afm.org">
+              Local389@afm.org
+            </H31Link4>
+          </H31LayoutCol>
+          <H31LayoutCol xs={1} />
+          <H31LayoutCol xs={7}>
+            <H31GoogleMap1 />
+          </H31LayoutCol>
+          <H31LayoutCol xs={1} />
+        </H31LayoutRow>
+      </H31LayoutContainer>
+    </MediaQuery>
+    <MediaQuery query="(max-width: 1199px)">
+      <H31LayoutContainer>
+        <H31LayoutRow>
+          <H31LayoutCol />
+        </H31LayoutRow>
+      </H31LayoutContainer>
+    </MediaQuery>
+    <MediaQuery query="(max-width: 767px)">
+      <H31LayoutContainer />
+    </MediaQuery>
   </Layout>
 );
