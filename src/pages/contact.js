f3604060dad4b0a62d20096de025fc0a1ac216fe
Matt <mattpezzente@gmail.com>
2019-09-10 16:29:12 -0400

    Contact page fixed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { css } from "@emotion/core";
-import styled from "@emotion/styled";
-import Responsive from "react-responsive";
+// import styled from "@emotion/styled";
 
 // Config
 import _config from "../components/_config";
@@ -23,21 +22,16 @@ import {
 // Images
 import ImgMastheadContact from "../components/images/mastheads/ImgMastheadContact";
 
-// Media Queries
-const Desktop = props => <Responsive {...props} minWidth={992} />;
-const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
-const Mobile = props => <Responsive {...props} maxWidth={767} />;
-
 // Grid Containers
-const GridContainer = styled.section`
-  padding: 0 180px;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 100px 100px 100px;
-  @media (max-width: 575px) {
-    padding: 0 50px;
-  }
-`;
+// const GridContainer = styled.section`
+//   padding: 0 180px;
+//   display: grid;
+//   grid-template-columns: 1fr 1fr;
+//   grid-template-rows: 100px 100px 100px;
+//   @media (max-width: 575px) {
+//     padding: 0 50px;
+//   }
+// `;
 
 export default () => (
   <Layout>
@@ -64,75 +58,97 @@ export default () => (
       titleSmall="Central FL Musicians Association"
       titleLarge="Contact Information"
     />
-    <Desktop>
-      <H31LayoutContainer
-        fluid
-        as="section"
-        css={css`
+
+    <H31LayoutContainer
+      fluid
+      as="section"
+      css={css`
+        margin: 45px 0 0 0;
+        @media (min-width: 992px) {
           margin: 90px 0;
-        `}
-      >
-        <H31LayoutRow>
-          <H31LayoutCol xs={1} />
-          <H31LayoutCol xs={2}>
-            <H31Title3A
-              css={css`
-                margin-bottom: 15px;
-                color: ${_config.colorSecondary};
-              `}
-            >
-              Mailing Address
-            </H31Title3A>
-            <H31Link4
-              css={css`
-                display: block;
-                margin-bottom: 60px;
-              `}
-              href="https://www.google.com/maps/place/3020+E+Robinson+St,+Orlando,+FL+32803/@28.5457854,-81.3470503,17z/data=!3m1!4b1!4m5!3m4!1s0x88e77ad22dc16cc1:0x85de0e8e8138f6ec!8m2!3d28.5457854!4d-81.3448616"
-            >
-              {["3020 East Robinson Street", <br />, "Orlando, FL 3280"]}
-            </H31Link4>
+        }
+      `}
+    >
+      <H31LayoutRow>
+        {/* <H31LayoutCol xs={1} /> */}
+        <H31LayoutCol
+          css={css`
+            text-align: center;
+            @media (min-width: 992px) {
+              text-align: left;
+            }
+          `}
+          xs={12}
+          lg={{ offset: 1, span: 3 }}
+        >
+          <H31Title3A
+            css={css`
+              margin-bottom: 15px;
+              color: ${_config.colorSecondary};
+            `}
+          >
+            Mailing Address
+          </H31Title3A>
+          <H31Link4
+            css={css`
+              display: block;
+              margin-bottom: 60px;
+            `}
+            href="https://www.google.com/maps/place/3020+E+Robinson+St,+Orlando,+FL+32803/@28.5457854,-81.3470503,17z/data=!3m1!4b1!4m5!3m4!1s0x88e77ad22dc16cc1:0x85de0e8e8138f6ec!8m2!3d28.5457854!4d-81.3448616"
+          >
+            {["3020 East Robinson Street", <br />, "Orlando, FL 3280"]}
+          </H31Link4>
 
-            <H31Title3A
-              css={css`
-                margin-bottom: 15px;
-                color: ${_config.colorSecondary};
-              `}
-            >
-              Number
-            </H31Title3A>
-            <H31Link4
-              css={css`
-                display: block;
-                margin-bottom: 60px;
-              `}
-              href="tel::(407) 894-8666"
-            >
-              (407) 894-8666
-            </H31Link4>
+          <H31Title3A
+            css={css`
+              margin-bottom: 15px;
+              color: ${_config.colorSecondary};
+            `}
+          >
+            Number
+          </H31Title3A>
+          <H31Link4
+            css={css`
+              display: block;
+              margin-bottom: 60px;
+            `}
+            href="tel::(407) 894-8666"
+          >
+            (407) 894-8666
+          </H31Link4>
 
-            <H31Title3A
-              css={css`
-                margin-bottom: 15px;
-                color: ${_config.colorSecondary};
-              `}
-            >
-              E-mail
-            </H31Title3A>
-            <H31Link4 href="mailto::Local389@afm.org">
-              Local389@afm.org
-            </H31Link4>
-          </H31LayoutCol>
-          <H31LayoutCol xs={1} />
-          <H31LayoutCol xs={7}>
-            <H31GoogleMap1 />
-          </H31LayoutCol>
-          <H31LayoutCol xs={1} />
-        </H31LayoutRow>
-      </H31LayoutContainer>
-    </Desktop>
+          <H31Title3A
+            css={css`
+              margin-bottom: 15px;
+              color: ${_config.colorSecondary};
+            `}
+          >
+            E-mail
+          </H31Title3A>
+          <H31Link4 href="mailto::Local389@afm.org">Local389@afm.org</H31Link4>
+        </H31LayoutCol>
+        {/* <H31LayoutCol xs={1} /> */}
+        <H31LayoutCol
+          css={css`
+            margin-top: 45px;
+            padding: 0;
+            height: 350px;
+            @media (min-width: 992px) {
+              margin-top: 0;
+              padding: 0 15px;
+              height: 330px;
+            }
+          `}
+          xs={12}
+          lg={7}
+        >
+          <H31GoogleMap1 />
+        </H31LayoutCol>
+        {/* <H31LayoutCol xs={1} /> */}
+      </H31LayoutRow>
+    </H31LayoutContainer>
 
-    <Tablet>
+    {/* <Tablet>
       <H31LayoutContainer
         fluid
         as="section"
@@ -200,9 +216,9 @@ export default () => (
       >
         <H31GoogleMap1 />
       </H31LayoutContainer>
-    </Tablet>
+    </Tablet> */}
 
-    <Mobile>
+    {/* <Mobile>
       <H31LayoutContainer
         fluid
         as="section"
@@ -276,6 +292,6 @@ export default () => (
       >
         <H31GoogleMap1 />
       </H31LayoutContainer>
-    </Mobile>
+    </Mobile> */}
   </Layout>
 );
