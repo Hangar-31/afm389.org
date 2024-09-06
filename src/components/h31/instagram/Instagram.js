b19c0391801f26129a1a80db30e588f8d3cda045
AFM389 <info@afm389.org>
2024-09-06 11:21:10 -0400

    Update Instagram.js

--- src/components/h31/instagram/Instagram.js
+++ src/components/h31/instagram/Instagram.js
@@ -62,7 +62,7 @@ const Wrapper = styled.section`
   }
 `;
 
-const instIds = ["C_f3Cm4uusJ", "C-aVjEntG8T", "C-dPzWggFSi", "C_dYRqONz04"];
+const instIds = ["C_ixvmqvstU", "C_f3Cm4uusJ", "C-aVjEntG8T", "C-dPzWggFSi"];
 
 function InstagramSection() {
   return (
